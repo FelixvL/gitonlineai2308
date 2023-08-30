2e1d47586e758bf55dce8865b90ee44b333a85d6
Lon Lrn <lonlrn@Galaxy-A40-van-Tom.2e.lan>
2023-08-30 14:21:22 +0200

    eerste

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS, cross_origin
+import felixbestand
+
+app = Flask(__name__)
+cors = CORS(app)
+
+@app.route("/")
+def hello_world():
+
+    return "<p>Hello, World!</p>"
+
+@app.route("/tweede/<destad>")
+def hello_world2(destad):
+
+    return felixbestand.ditismijnfunctie(destad)
